0e193c50f779bf2ade213bb7da2a0d643442d5b0
hosein boobaee <m.hossein.boobaee@gmail.com>
2024-06-21 16:25:40 +0330

    14030401

--- _inc/widgets/sidebar-widget.php
+++ _inc/widgets/sidebar-widget.php
@@ -7,7 +7,7 @@ function register_sidebar_widgets(){
             'description'=>'ساید بار سفارشی قالب',
             'before_widget'=>'<li id="%1$s" class="%2$s">',
             'after_widget'=>'</li>',
-            'before_title'=> '<h2 class="widget-title">',
+            'before_title'=> '<h2 class="title">',
             'after_title'=> '</h2>'
         ]
         );
